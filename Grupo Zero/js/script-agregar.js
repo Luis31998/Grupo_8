243eb68dd89902f0848952367b1e7c15a5e5e5e1
Luis31998 <xinoavaldivia@gmail.com>
2021-05-16 17:52:28 -0400

    mensaje error agregar obra

---
+++ Grupo Zero/js/script-agregar.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+    $("#error-titulo").hide()
+
+    $("#titulo-agre").blur(function () {
+
+
+        //VALIDACIÓN Titulo
+        if ($("#titulo-agre").val().length < 3) {
+            $("#error-titulo").html("El titulo de la obra esta vacio")
+            $("#error-titulo").fadeIn()
+        }
+
+    });
+
+    $("#error-comentarios").hide()
+
+    $("#coment-agre").blur(function () {
+
+
+        //VALIDACIÓN DESCRIPCION
+        if ($("#coment-agre").val().length < 3) {
+            $("#error-comentarios").html("Comente la obra")
+            $("#error-comentarios").fadeIn()
+        }
+
+    });
+
+
+
+});
